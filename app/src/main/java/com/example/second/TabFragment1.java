34a0e1b25cfc6ab5f6f8ef43bcf9ae1bd293bb06
dormkim <ehals5921@gmail.com>
2019-07-05 03:59:34 +0900

    Image Complete

--- app/src/main/java/com/example/second/TabFragment1.java
+++ app/src/main/java/com/example/second/TabFragment1.java
@@ -8,14 +8,18 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
+import android.widget.ImageView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -24,12 +28,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.futuremind.recyclerviewfastscroll.FastScroller;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.bson.internal.Base64;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class TabFragment1 extends Fragment{
@@ -38,12 +45,34 @@ public class TabFragment1 extends Fragment{
     private RecyclerView.LayoutManager mLayoutManager;
     private ArrayList<ContactRecyclerItem> mMyData;
     private View view;
+    private JSONObject all_contact;
+    private Button posttest;
+    private Button gettest;
+    private ImageView test;
     private static final int ADD_CONTACT = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_1, container, false);
         mMyData = getContactList();
+        all_contact = ArrListToJObj(mMyData, "All");
+        posttest = (Button) view.findViewById(R.id.postTest);
+        gettest = (Button) view.findViewById(R.id.getTest);
+
+        posttest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new JSONTaskPost().execute("http://143.248.38.46:8080/api/contacts");
+            }
+        });
+
+        gettest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new JSONTaskGet().execute("http://143.248.38.46:8080/api/contacts/5d1e4c13e1af7459d4a22059");
+            }
+        });
+
         return view;
     }
     @Override
@@ -123,22 +152,32 @@ public class TabFragment1 extends Fragment{
                 ContactRecyclerItem contactItem;
                 contactItem = arrList.get(i);
 
+                Drawable temp = contactItem.getIcon();
+                Bitmap bitmap = ((BitmapDrawable)temp).getBitmap();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                byte[] bitmapdata = stream.toByteArray();
+                String str = Base64.encode(bitmapdata);
+
                 JSONObject sObj = new JSONObject();
                 sObj.put("name", contactItem.getName());
                 sObj.put("phonenumber", contactItem.getPhone());
-                sObj.put("iconID", contactItem.getPhone());
-                sObj.put("pID", contactItem.getPersonID());
-                sObj.put("iconDrawable", contactItem.getIcon());
+                sObj.put("bitmapInfo",str);
+                //sObj.put("iconID", contactItem.getPhone());
+                //sObj.put("pID", contactItem.getPersonID());
+                //sObj.put("contactID",contactItem.getContentId());
+                //sObj.put("iconDrawable", contactItem.getIcon());
                 jArray.put(sObj);
             }
-            obj.put("filename", name);
-            obj.put("ContactData", jArray);
+            obj.put("name", name);
+            obj.put("phonenumber", jArray);
 
         }catch (JSONException e){
             e.printStackTrace();
         }
         return obj;
     }
+
 
      public ArrayList<ContactRecyclerItem> getContactList(){
          Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
@@ -229,5 +268,166 @@ public class TabFragment1 extends Fragment{
         mMyData.remove(position);
         mAdapter.notifyItemRemoved(position);
         onResume();
+    }
+
+    public class JSONTaskPost extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String urls[]) {
+            try {
+                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+                JSONObject jsonObject = all_contact;
+
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try {
+                    //URL url = new URL("http://192.168.25.16:3000/users");
+                    URL url = new URL(urls[0]);
+                    //연결을 함
+                    con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("POST");//POST방식으로 보냄
+                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                    con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
+                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                    con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                    con.connect();
+
+                    //서버로 보내기위해서 스트림 만듬
+                    OutputStream outStream = con.getOutputStream();
+                    //버퍼를 생성하고 넣음
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();//버퍼를 받아줌
+
+                    //서버로 부터 데이터를 받음
+                    InputStream stream = con.getInputStream();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer = new StringBuffer();
+
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line);
+                    }
+
+                    return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (con != null) {
+                        con.disconnect();
+                    }
+                    try {
+                        if (reader != null) {
+                            reader.close();//버퍼를 닫아줌
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //tvData.setText(result);//서버로 부터 받은 값을 출력해주는 부
+        }
+    }
+
+    public class JSONTaskGet extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String urls[]) {
+            try {
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+
+                try {
+                    URL url = new URL(urls[0]);//url을 가져온다.
+                    con = (HttpURLConnection) url.openConnection();
+                    con.connect();//연결 수행
+
+                    //입력 스트림 생성
+                    InputStream stream = con.getInputStream();
+
+                    //속도를 향상시키고 부하를 줄이기 위한 버퍼를 선언한다.
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    //실제 데이터를 받는곳
+                    StringBuffer buffer = new StringBuffer();
+
+                    //line별 스트링을 받기 위한 temp 변수
+                    String line = "";
+
+                    //아래라인은 실제 reader에서 데이터를 가져오는 부분이다. 즉 node.js서버로부터 데이터를 가져온다.
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line);
+                    }
+
+                    //다 가져오면 String 형변환을 수행한다. 이유는 protected String doInBackground(String... urls) 니까
+                    parseJsonData(buffer.toString());
+                    return buffer.toString();
+
+                    //아래는 예외처리 부분이다.
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    //종료가 되면 disconnect메소드를 호출한다.
+                    if (con != null) {
+                        con.disconnect();
+                    }
+                    try {
+                        //버퍼를 닫아준다.
+                        if (reader != null) {
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }//finally 부분
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    private void parseJsonData(String jsonResponse){
+        try
+        {
+            test = (ImageView) view.findViewById(R.id.test);
+            JSONObject jsonObject = new JSONObject(jsonResponse);
+            JSONArray jsonArray = (JSONArray) jsonObject.get("phonenumber");
+
+            for(int i=0;i<jsonArray.length();i++)
+            {
+                JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                String value1 = jsonObject1.getString("name");
+                String value2 = jsonObject1.getString("phonenumber");
+                String value3 = jsonObject1.getString("bitmapInfo");
+                byte[] temp = Base64.decode(value3);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(temp, 0, temp.length);
+                test.setImageBitmap(bitmap);
+                Thread.sleep(3000);
+            }
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
